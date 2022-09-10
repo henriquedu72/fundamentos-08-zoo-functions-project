e8134255b5fb8dfb61229eb7b90c5eab61833abb
eduardo <duh_henrique@hotmail.com>
2022-09-10 18:02:00 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const countAnimals = (animal) => {
   // seu código aqui
-}
+  if (animal === undefined) {
+    const result = {};
+    species.filter((specie) => {
+      const newObject = { [specie.name]: specie.residents.length };
+      return Object.assign(result, newObject);
+    }); return result;
+  }
+  const specieOfAnimal = species.find((specie) => animal.specie === specie.name);
+  const sexOfSpecies = specieOfAnimal.residents.filter((resident) => resident.sex === animal.sex);
+  return (animal.sex) ? sexOfSpecies.length : specieOfAnimal.residents.length;
+};
 
 module.exports = countAnimals;
