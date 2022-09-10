7efc75fb230b7f4eac30aeac8cfebc12345fc14a
eduardo <duh_henrique@hotmail.com>
2022-09-10 16:20:42 -0300

    corrige 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,10 +1,10 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => data.employees.some((employee) => employee.managers.includes(id));
+const isManager = (id) => employees.some((employee) => employee.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
   if (isManager(managerId)) {
-    const emp = data.employees.filter((employee) => employee.managers.includes(managerId));
+    const emp = employees.filter((employee) => employee.managers.includes(managerId));
     return emp.map((employee) => `${employee.firstName} ${employee.lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
